3396d3be47ca45931cdf8eae0539f55dcfafc31f
Robert Parkhurst <raparkhurst@digitalsynapse.io>
2018-02-07 15:20:27 -0600

    Updated site for editing forms.  Worked more on UI.

--- alexandria/app/controllers/books_controller.rb
+++ alexandria/app/controllers/books_controller.rb
@@ -1,10 +1,24 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.order('id')
   end
 
 
   def show
     @book = Book.find(params[:id])
   end
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    book_params = params.require(:book).permit(:name, :description, :chapter, :part_of_series, :date_start, :date_end, :paid)
+    @book.update(book_params)
+    redirect_to @book
+  end
+
 end
--- alexandria/config/routes.rb
+++ alexandria/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "books" => "books#index"
-  get "books/:id" => "books#show", as: "book"
+  # root URL -- for now setting to index, but will change once we get more stuff bolted on
+  root    "books#index"
+
+
+  # Books routes
+  #get     "books"               => "books#index"
+  #get     "books/:id"           => "books#show", as: "book"
+  #get     "books/:id/edit"      => "books#edit", as: "edit_book"
+  #patch   "books/:id"           => "books#update"
+  resources :books
+
 end
