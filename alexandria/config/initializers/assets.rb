ea3317fc8a9f826f8bfc2a08c72b79ddb2936b95
Robert Parkhurst <raparkhurst@digitalsynapse.io>
2018-03-05 22:15:36 -0600

    updating admin-lte integration.  After fixing some issues with CSS (not sure what, but it works now), I'm going to use this moving forward.  Next Steps:  get tests working then merge back into develop branch

--- alexandria/config/initializers/assets.rb
+++ alexandria/config/initializers/assets.rb
@@ -8,11 +8,13 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 
 # Configuring App to look at Bower and NPM locations.
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'node_modules')
+#Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'node_modules')
 
 
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( jquery/dist/jquery.min.js bootstrap/dist/js/bootstrap.min.js fastclick/lib/fastclick.js admin-lte/dist/js/adminlte.min.js )
+#Rails.application.config.assets.precompile += %w( jquery/dist/jquery.min.js bootstrap/dist/js/bootstrap.min.js fastclick/lib/fastclick.js admin-lte/dist/js/adminlte.min.js )
+
+Rails.application.config.assets.precompile += %w( application_back.css application_back.js )
