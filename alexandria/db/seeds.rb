707bed1863813523c17db68915ff914c44d32ba8
Robert Parkhurst <raparkhurst@digitalsynapse.io>
2018-06-25 13:55:06 -0500

    populating seeds.rb -- these are all dummy passwords

--- alexandria/db/seeds.rb
+++ alexandria/db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if !User.exists?(username: "61Mv7nNK6K")
+  admin = User.new({
+                       username: "61Mv7nNK6K",
+                       firstname: "The",
+                       lastname: "Architect",
+                       email: "support@digitalsynapse.io",
+                       password: "zKwanusojq1kv8Ywg6tswijrTduuWu6du",
+                       password_confirmation: "zKwanusojq1kv8Ywg6tswijrTduuWu6du"
+                   })
+
+  admin.toggle!(:admin)
+
+
+  if admin.valid?
+    admin.save()
+
+  elsif admin.errors.any?
+    admin.errors.full_messages.each do |msg|
+      puts msg
+    end
+  else
+    puts "****NOT VALID****"
+  end
+end
+
+
+if !User.exists?(username: "admin")
+  dummyAdmin1 = User.create({
+                           username: "admin",
+                           firstname: "admin",
+                           lastname: "user",
+                           email: "support@digitalsynapse.io",
+                           password: "tymp3r2fqurxy15efYwtjjngkzHuCpQyi",
+                           password_confirmation: "tymp3r2fqurxy15efYwtjjngkzHuCpQyi"
+                       })
+end
+
+if !User.exists?(username: "root")
+  dummyAdmin2 = User.create({
+                             username: "root",
+                             firstname: "admin",
+                             lastname: "user",
+                             email: "support@digitalsynapse.io",
+                             password: "pa0hprj7cw6gunYj6dhxqrnwwRqiXrRjz",
+                             password_confirmation: "pa0hprj7cw6gunYj6dhxqrnwwRqiXrRjz"
+                         })
+end
